86e623d8070107eb3cda72a15015ce42ec404f88
Miss-Cheese <nat.galligan@gmail.com>
2020-08-18 19:08:39 -0400

    more recursion practice done

--- recursionPractice.js
+++ recursionPractice.js
@@ -26,3 +26,52 @@ function fib(num){
     if (num === 1 || num === 2) return 1
     return fib(num - 1) + fib(num - 2)
 }
+
+function reverse(str) {
+
+    if (str.length === 1) return str
+
+    // return str[str.length - 1] + reverse(str.slice(0, str.length - 1))
+
+    // cleaner way
+    return reverse(str.slice(1)) + str[0]
+}
+
+function isPalindrome(str){
+    // if the first and the last character are the same - keep invoking the function 
+    // until there are less than 2 characters left in the string
+
+    if (str.length < 2) return true
+    else if (str[0] === str[str.length - 1]) {
+        return isPalindrome(str.slice(1, str.length - 1))
+    } else {
+        return false
+    }
+}
+
+function someRecursive(arr, callback){
+  
+    if (arr.length === 0) {
+        return false
+    }
+
+    if (callback(arr[0])) {
+        return true
+    } else {
+        return someRecursive(arr.slice(1), callback)
+    }
+}
+
+function flatten(arr){
+    let newArr = []
+
+    for (let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+            newArr = newArr.concat(flatten(arr[i]))
+        } else {
+            newArr.push(arr[i])
+        }
+    }
+
+    return newArr
+}
